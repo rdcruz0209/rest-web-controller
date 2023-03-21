46fcdab4c8d513f635d653634f99a910c0c25666
rdcruz0209 <robert.delacruz119@gmail.com>
2023-03-21 23:20:09 +0800

    Implemented Response exception message to simplify the response for the consumer,

--- src/main/java/com/portfolioprojects/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/portfolioprojects/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,8 @@
 package com.portfolioprojects.rest.webservices.restfulwebservices.user;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 
@@ -8,11 +11,15 @@ import java.time.LocalDate;
 @Getter
 @Setter
 //@NoArgsConstructor
+//@NoArgsConstructor if this class is to be used as
+// autowiring candidate in CustomizedResponseEntityExceptionHandler
 @AllArgsConstructor
 @ToString
 public class User {
-
     private int id;
+
+    @Size(min = 2, message = "Please input at least 2 characters for name")
     private String name;
+    @PastOrPresent(message = "Future date is not accepted for birthday field")
     private LocalDate birthDate;
 }
